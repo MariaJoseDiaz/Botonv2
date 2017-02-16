1d184ce5c37a2ba46eab5f4e69ecf31ae8434477
MariaJoseDiaz <mjdiaz9@uc.cl>
2017-02-16 13:18:07 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -47,7 +47,11 @@ function mousePressed () { // function touchStarted () {
   
   if (mouseX >= a-30 && mouseX <= a-30 + rect_widht && mouseY >= b-8 && mouseY <= b-8 + rect_height){
  fondo = color(random(0, 170), random(0, 170), random(0, 170));
-  
+  } else if (magnitude <= 6){
+         image(myImage1,0,0,windowWidth,windowHeight);
+     } else {
+         image(myImage2,0,0,windowWidth,windowHeight);
+     }
 
 } else if (mouseX >= c-30 && mouseX <= c-30 + rect_widht && mouseY >= b-8 && mouseY <= b-8 + rect_height){
  //fondo = color(random(0, 170), random(0, 170), random(0, 170)); 
@@ -145,7 +149,7 @@ function draw () {
     var r = energy * 2; 
   
   
-    translate(0, -40);
+    translate(0, height/10);
     noFill();
     stroke(0);
     strokeWeight(1);
